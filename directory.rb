63875f9de8fe5a01c5fa63d669c5c688f5bb8a93
Melanie Anderson <melanieanderson1995@gmail.com>
2020-06-29 15:18:25 +0100

    Made list interactive

--- directory.rb
+++ directory.rb
@@ -1,4 +1,23 @@
 #first we print the list of students
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  #get the first names
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of input_students
+  students
+end
+
+=begin
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -12,19 +31,25 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november},
 ]
+=end
+
 def print_header
   puts "The students of Villains Academy"
   puts "----------"
 end
+
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
