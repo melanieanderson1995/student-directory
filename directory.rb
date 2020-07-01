b22874e08288448d03605415b74afddaf5b30ffd
Melanie Anderson <melanieanderson1995@gmail.com>
2020-07-01 14:08:08 +0100

    Directory Final

--- directory.rb
+++ directory.rb
@@ -1,24 +1,3 @@
-def save_students
-  #open the file for writing
-  file = File.open("students.csv", "w")
-  #iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
 @students = []
 
 def print_menu
@@ -32,7 +11,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -55,24 +34,24 @@ def process(selection)
   end
 end
 
-def show_students
-  print_header
-  print_students_list(@students)
-  print_footer
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #get the first names
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     #get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -80,7 +59,7 @@ def print_header
   puts "----------"
 end
 
-def print_students_list(students)
+def print_student_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -90,6 +69,39 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exists?(filename)
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
 #nothing happens until we call the methods
+try_load_students
 interactive_menu
-@students = input_students
